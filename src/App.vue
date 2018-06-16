30d8498a83b08f553ecba6122f41951214a63732
volodko <a.f1ghter.v@gmail.com>
2018-06-16 03:12:58 +0300

    smallfix

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,8 @@
       <router-link to="/" class="homelink">home</router-link>
       <router-link to="/test" class="homelink">FireBase</router-link>
       <router-link to="/login" class="homelink">Login</router-link>
-      <router-link to="/dashboard" class="homelink">Dashboard</router-link>
       <router-link to="/test" class="homelink">FireBase</router-link>
       <router-link to="/admin" class="homelink">Admin</router-link>
-      <!-- <span>/</span>
-      <router-link to="/book" class="homelink">book</router-link> -->
     </nav>
 
     <router-view/>
@@ -22,43 +19,43 @@ export default {
 </script>
 
 <style lang="scss">
-// variables
-$c-main: #444;
-$c-accent: #B70000;
-$f-stack: 'Roboto', Helvetica, Arial, sans-serif;
-// eof variables
+  // variables
+  $c-main: #444;
+  $c-accent: #B70000;
+  $f-stack: 'Roboto', Helvetica, Arial, sans-serif;
+  // eof variables
 
-#app {
-  font-family: $f-stack;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: $c-main;
-  margin-top: 60px;
-
-}
-nav {
-  margin-bottom: 30px;
-}
-img {
-  max-width: 100%;
-}
-nav {
-  text-align: center;
-  font-size: 28px;
-}
-.homelink {
-  margin: 0 auto;
-  color: $c-accent;
-  font-weight: 300;
-  text-decoration: none;
-  text-align: center;
-  font-style: italic;
-
-  &.router-link-active {
+  #app {
+    font-family: $f-stack;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: $c-main;
+    margin-top: 60px;
 
   }
-  &.router-link-exact-active {
-    border-bottom: 1px solid currentColor;
+  nav {
+    margin-bottom: 30px;
   }
-}
+  img {
+    max-width: 100%;
+  }
+  nav {
+    text-align: center;
+    font-size: 28px;
+  }
+  .homelink {
+    margin: 0 auto;
+    color: $c-accent;
+    font-weight: 300;
+    text-decoration: none;
+    text-align: center;
+    font-style: italic;
+
+    &.router-link-active {
+
+    }
+    &.router-link-exact-active {
+      border-bottom: 1px solid currentColor;
+    }
+  }
 </style>
